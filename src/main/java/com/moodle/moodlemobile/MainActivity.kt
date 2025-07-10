349ef8384271763f91df35101b1e20fda8600ef9
Akshaya <32mins.where@gmail.com>
2025-07-10 13:15:39 +0530

    Initial commit

---
+++ src/main/java/com/moodle/moodlemobile/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.moodle.moodlemobile
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private val websiteUrl = "https://digiviscom.com/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            builtInZoomControls = true
+            displayZoomControls = false
+            setSupportZoom(true)
+            defaultTextEncodingName = "utf-8"
+            userAgentString = "${userAgentString} DigiviscomApp"
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(websiteUrl)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
